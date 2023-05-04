22c01410bf5fb9244a9ace41828d27e4b934f4d0
Anastasiia Kupriianets <94451253+AnanasikA@users.noreply.github.com>
2023-05-04 12:11:49 +0200

    Add files via upload

---
+++ OO_LightSwitch_with_Test_Code.py
@@ -0,0 +1,27 @@
+class LightSwitch():
+    def __init__(self):
+        self.switchIsOn = False
+
+    def turnOn(self):
+        self.switchIsOn = True
+
+    def turnOff(self):
+        self.switchIsOn = False
+
+    def show(self):
+        print(self.switchIsOn)
+
+# kod główny
+
+
+oLightSwitch = LightSwitch()
+
+# Wywołanie metod
+
+oLightSwitch.show()
+oLightSwitch.turnOn()
+oLightSwitch.show()
+oLightSwitch.turnOff()
+oLightSwitch.show()
+oLightSwitch.turnOn()
+oLightSwitch.show()
